8c854aa83211e3bf439c87ec8fbbbc65da3e6ad0
AinAyona Johnson <ayonaj212@GMAIL.COM>
2026-05-28 13:54:48 -0400

    step 5

--- py-hw.py
+++ py-hw.py
@@ -3,11 +3,11 @@ def main():
 	print("This program illustrates a chaotic function")
 
 	x = eval(input("Enter a number between 1 and 2: "))
-	print("hello")
+	k = eval(input("Enter a number for our k value "))
 
 	for i in range(10):
 
-		x = 3.9 * x * (1 - x)
+		x = k * x * (1 - x)
 		print(x)
 
 main()
